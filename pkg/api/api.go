f55a1298beeee49143e10f849662dece22d00a8e
moskvinspace <me@moskvin.space>
2023-05-04 21:16:13 +0300

    add enpoint sign-up & sign-in

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -2,17 +2,16 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/moskvinspace/simple-web-app/pkg/api/handlers"
 	"log"
 	"os"
 )
 
 func Start() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	r.POST("/sign-up", handlers.SignUp)
+	r.POST("/sign-in", handlers.SignIn)
 
 	if err := r.Run(); err != nil {
 		log.Fatal("Failed to run server. \n", err)
