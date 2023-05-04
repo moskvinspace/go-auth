e8c95ea96cb341223708b1ac0608b39017d44034
moskvinspace <me@moskvin.space>
2023-05-04 17:41:13 +0300

    init pkg\database

---
+++ pkg/database/database.go
@@ -0,0 +1,31 @@
+package database
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+)
+
+var DB *gorm.DB
+
+func ConnectDb() {
+	dsn := fmt.Sprintf(
+		"host=database user=%s password=%s dbname=%s port=5432 sslmode=disable",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+	if err != nil {
+		log.Fatal("Failed to connect to database. \n", err)
+		os.Exit(2)
+	}
+
+	DB = db
+}
