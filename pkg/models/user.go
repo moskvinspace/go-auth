e9d499b46f34b863560987876c5cdf70ff082c71
moskvinspace <me@moskvin.space>
2023-05-04 22:23:39 +0300

    fix userModel

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -10,7 +10,7 @@ type User struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
-	Password  string
+	Password  string `json:""`
 }
 
 func (u *User) Create() error {
