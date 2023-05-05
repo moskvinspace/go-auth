67e284a1690368a39ca04adc0d96f6a89884d086
moskvinspace <me@moskvin.space>
2023-05-05 21:44:38 +0300

    fix

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -11,26 +11,12 @@ import (
 
 var DB *gorm.DB
 
-type dbConfig struct {
-	host     string
-	user     string
-	password string
-	dbname   string
-	port     string
-	sslmode  string
-}
-
 func ConnectDB() {
-	cfg := newConfig()
-
 	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
-		cfg.host,
-		cfg.user,
-		cfg.password,
-		cfg.dbname,
-		cfg.port,
-		cfg.sslmode,
+		"host=database user=%s password=%s dbname=%s port=5432 sslmode=disable",
+		os.Getenv("POSTGRES_USER"),
+		os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_DB"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
@@ -42,40 +28,4 @@ func ConnectDB() {
 	}
 
 	DB = db
-}
-
-func defaultConfig() *dbConfig {
-	return &dbConfig{
-		host:     "localhost",
-		user:     "postgres",
-		password: "postgres",
-		dbname:   "postgres",
-		port:     "5432",
-		sslmode:  "disable",
-	}
-}
-
-func newConfig() *dbConfig {
-	cfg := defaultConfig()
-
-	host := os.Getenv("HOST")
-	user := os.Getenv("POSTGRES_USER")
-	password := os.Getenv("POSTGRES_PASSWORD")
-	dbname := os.Getenv("POSTGRES_DB")
-
-	switch {
-	case host != "":
-		cfg.host = host
-		fallthrough
-	case user != "":
-		cfg.user = user
-		fallthrough
-	case password != "":
-		cfg.password = password
-		fallthrough
-	case dbname != "":
-		cfg.dbname = dbname
-	}
-
-	return cfg
 }
