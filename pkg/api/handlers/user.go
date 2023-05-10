0ce315b269bafd0ecbe5f885a2f9dced7d93a7d9
moskvinspace <me@moskvin.space>
2023-05-10 23:41:49 +0300

    add handler current_user

---
+++ pkg/api/handlers/user.go
@@ -0,0 +1,42 @@
+package handlers
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	"github.com/moskvinspace/simple-web-app/pkg/models"
+	"net/http"
+)
+
+type currentUserResponse struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string `json:"email"`
+}
+
+func CurrentUser(c *gin.Context) {
+	cookie, err := c.Cookie("jwt")
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{})
+		return
+	}
+
+	token, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte(SecretKey), nil
+	})
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{})
+		return
+	}
+
+	claims := token.Claims.(*jwt.StandardClaims)
+	user, err := models.GetUser("id", claims.Issuer)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+	}
+
+	c.JSON(http.StatusOK, currentUserResponse{
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+	})
+}
